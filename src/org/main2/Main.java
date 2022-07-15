f56010afb504cd263139dee473299f2397b5d123
ahndaeun <dean034@naver.com>
2022-07-15 17:30:57 +0900

    예제 숙제

--- src/org/main2/Main.java
+++ src/org/main2/Main.java
@@ -3,19 +3,27 @@ package org.main2;
 public class Main {
 public static void main(String[] args) {
 	
-	
-	//1. com. utll. CollextionStack
+	// test data set
+	int [] nums= {1,2,3};
+	//1. com. utll. CollectionStack
 	// 	 add - 새로운 숫자 추가 무조건 0번 인데스 추가(앞에다가 넣어) 입력받아 
 	// 	 remove 호출- 0번 인덱스 삭제
+	//	 collectionstack.add -> {4,1,2,3}
+	//	 collectionstack.remove -> 	 {2,3}
 	
 	
 	
-	//	 2. com. utll. CollextionStionQue
+	//	 2. com. utll. CollectionStionQue
 		// add - 새로운 숫자 추가 무조건 0번 인데스 추가(앞에다가 넣어) 입력받아 
 		// remove- 마지막 인덱스 번호 삭제 
-
-	//	 2. com. utll. CollextionStionSet
+	//	 collectionQue.add -> {4,1,2,3}
+	//	 collectionQue.remove -> 	 {1,2}
+	
+	
+	//	 3. com. utll. CollectionStionSet
 		//  add - 새로운 숫자 추가 무조건 마지막에 추가  (단, 중복되지 않는 숫자만 추가	
-		//  remove- 삭제할 숫자를 입력받아서 일치하는 숫자 삭제  
+		//  remove- 삭제할 숫자를 입력받아서 일치하는 숫자 삭제
+		//	 CollectionStionSet.add -> {1,2,3,4}
+		//	 CollectionStionSet.remove -> 	 {2,3}
 }
 }
--- src/org/untill/CollectionStack/CollectionStack.java
+++ src/org/untill/CollectionStack/CollectionStack.java
@@ -1,5 +1,11 @@
 package org.untill.CollectionStack;
 
+import java.util.Scanner;
+
 public class CollectionStack {
 
+	
+	Scanner sc= new Scanner(System.in);
+	
+	
 }
