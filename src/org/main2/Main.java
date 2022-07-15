92c803533191b871a9aa10cd73779d7595e70804
ahndaeun <dean034@naver.com>
2022-07-15 17:16:21 +0900

    7월15일 5

---
+++ src/org/main2/Main.java
@@ -0,0 +1,21 @@
+package org.main2;
+
+public class Main {
+public static void main(String[] args) {
+	
+	
+	//1. com. utll. CollextionStack
+	// 	 add - 새로운 숫자 추가 무조건 0번 인데스 추가(앞에다가 넣어) 입력받아 
+	// 	 remove 호출- 0번 인덱스 삭제
+	
+	
+	
+	//	 2. com. utll. CollextionStionQue
+		// add - 새로운 숫자 추가 무조건 0번 인데스 추가(앞에다가 넣어) 입력받아 
+		// remove- 마지막 인덱스 번호 삭제 
+
+	//	 2. com. utll. CollextionStionSet
+		//  add - 새로운 숫자 추가 무조건 마지막에 추가  (단, 중복되지 않는 숫자만 추가	
+		//  remove- 삭제할 숫자를 입력받아서 일치하는 숫자 삭제  
+}
+}
---
+++ src/org/until/Collection.java
@@ -0,0 +1,11 @@
+
+package org.until;
+
+public interface Collection {
+
+	public abstract int [] add(int [] numbers);
+	public abstract int [] remove(int [] numbers);
+
+	
+	
+}
